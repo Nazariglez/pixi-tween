dd8dea5eb4c1f12c4315196eaf075f52b915445c
Nazarí González <nazari.nz@gmail.com>
2016-04-03 20:41:36 +0100

    now the elastic easing has the option to pass the values a, and p, to change lightly the effect. Some refactor, and added a new easing (in progress) to convert arrays in effects

--- src/Easing.js
+++ src/Easing.js
@@ -1,240 +1,248 @@
 const Easing = {
   linear: function(){
-    return function(k){
-      return k;
+    return function(t){
+      return t;
     };
   },
 
   inQuad: function(){
-    return function(k){
-      return k*k;
+    return function(t){
+      return t*t;
     };
   },
 
   outQuad: function(){
-    return function(k){
-      return k*(2-k);
+    return function(t){
+      return t*(2-t);
     };
   },
 
   inOutQuad: function(){
-    return function(k){
-      k *= 2;
-      if ( k < 1 ) return 0.5 * k * k;
-      return - 0.5 * ( --k * ( k - 2 ) - 1 );
+    return function(t){
+      t *= 2;
+      if ( t < 1 ) return 0.5 * t * t;
+      return - 0.5 * ( --t * ( t - 2 ) - 1 );
     };
   },
 
   inCubic: function(){
-    return function (k) {
-      return k * k * k;
+    return function (t) {
+      return t * t * t;
     };
   },
 
   outCubic: function(){
-    return function(k){
-      return --k * k * k + 1;
+    return function(t){
+      return --t * t * t + 1;
     };
   },
 
   inOutCubic: function(){
-    return function(k){
-      k *= 2;
-      if ( k < 1 ) return 0.5 * k * k * k;
-      k -= 2
-      return 0.5 * ( k * k * k + 2 );
+    return function(t){
+      t *= 2;
+      if ( t < 1 ) return 0.5 * t * t * t;
+      t -= 2
+      return 0.5 * ( t * t * t + 2 );
     };
   },
 
   inQuart: function(){
-    return function(k){
-      return k * k * k * k;
+    return function(t){
+      return t * t * t * t;
     };
   },
 
   outQuart: function(){
-    return function(k){
-      return 1 - ( --k * k * k * k );
+    return function(t){
+      return 1 - ( --t * t * t * t );
     };
   },
 
   inOutQuart: function(){
-    return function(k){
-      k *= 2;
-      if ( k < 1) return 0.5 * k * k * k * k;
-      k -= 2;
-      return - 0.5 * ( k * k * k * k - 2 );
+    return function(t){
+      t *= 2;
+      if ( t < 1) return 0.5 * t * t * t * t;
+      t -= 2;
+      return - 0.5 * ( t * t * t * t - 2 );
     };
   },
 
   inQuint: function(){
-    return function(k){
-      return k * k * k * k * k;
+    return function(t){
+      return t * t * t * t * t;
     };
   },
 
   outQuint: function(){
-    return function(k){
-      return --k * k * k * k * k + 1;
+    return function(t){
+      return --t * t * t * t * t + 1;
     };
   },
 
   inOutQuint: function(){
-    return function(k){
-      k *= 2;
-      if ( k < 1 ) return 0.5 * k * k * k * k * k;
-      k -= 2;
-      return 0.5 * ( k * k * k * k * k + 2 );
+    return function(t){
+      t *= 2;
+      if ( t < 1 ) return 0.5 * t * t * t * t * t;
+      t -= 2;
+      return 0.5 * ( t * t * t * t * t + 2 );
     };
   },
 
   inSine: function(){
-    return function(k){
-      return 1 - Math.cos( k * Math.PI / 2 );
+    return function(t){
+      return 1 - Math.cos( t * Math.PI / 2 );
     };
   },
 
   outSine: function(){
-    return function(k){
-      return Math.sin( k * Math.PI / 2 );
+    return function(t){
+      return Math.sin( t * Math.PI / 2 );
     };
   },
 
   inOutSine: function(){
-    return function(k){
-      return 0.5 * ( 1 - Math.cos( Math.PI * k ) );
+    return function(t){
+      return 0.5 * ( 1 - Math.cos( Math.PI * t ) );
     };
   },
 
   inExpo: function(){
-    return function(k){
-      return k === 0 ? 0 : Math.pow( 1024, k - 1 );
+    return function(t){
+      return t === 0 ? 0 : Math.pow( 1024, t - 1 );
     };
   },
 
   outExpo: function(){
-    return function(k){
-      return k === 1 ? 1 : 1 - Math.pow( 2, - 10 * k );
+    return function(t){
+      return t === 1 ? 1 : 1 - Math.pow( 2, - 10 * t );
     };
   },
 
   inOutExpo: function(){
-    return function(k){
-      if ( k === 0 ) return 0;
-      if ( k === 1 ) return 1;
-      k *= 2;
-      if ( k < 1 ) return 0.5 * Math.pow( 1024, k - 1 );
-      return 0.5 * ( - Math.pow( 2, - 10 * ( k - 1 ) ) + 2 );
+    return function(t){
+      if ( t === 0 ) return 0;
+      if ( t === 1 ) return 1;
+      t *= 2;
+      if ( t < 1 ) return 0.5 * Math.pow( 1024, t - 1 );
+      return 0.5 * ( - Math.pow( 2, - 10 * ( t - 1 ) ) + 2 );
     };
   },
 
   inCirc: function(){
-    return function(k){
-      return 1 - Math.sqrt( 1 - k * k );
+    return function(t){
+      return 1 - Math.sqrt( 1 - t * t );
     };
   },
 
   outCirc: function(){
-    return function(k){
-      return Math.sqrt( 1 - ( --k * k ) );
+    return function(t){
+      return Math.sqrt( 1 - ( --t * t ) );
     };
   },
 
   inOutCirc: function(){
-    return function(k){
-      k *= 2
-      if ( k < 1) return - 0.5 * ( Math.sqrt( 1 - k * k) - 1);
-      return 0.5 * ( Math.sqrt( 1 - (k - 2) * (k - 2)) + 1);
+    return function(t){
+      t *= 2
+      if ( t < 1) return - 0.5 * ( Math.sqrt( 1 - t * t) - 1);
+      return 0.5 * ( Math.sqrt( 1 - (t - 2) * (t - 2)) + 1);
     };
   },
 
 
-  inElastic: function(){
-    return function(k){
-      let s, a = 0.1, p = 0.4;
-      if ( k === 0 ) return 0;
-      if ( k === 1 ) return 1;
+  inElastic: function(a = 0.1,p = 0.4){
+    return function(t){
+      let s;
+      if ( t === 0 ) return 0;
+      if ( t === 1 ) return 1;
       if ( !a || a < 1 ) { a = 1; s = p / 4; }
       else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
-      return - ( a * Math.pow( 2, 10 * (k-1) ) * Math.sin( ( (k-1) - s ) * ( 2 * Math.PI ) / p ) );
+      return - ( a * Math.pow( 2, 10 * (t-1) ) * Math.sin( ( (t-1) - s ) * ( 2 * Math.PI ) / p ) );
     };
   },
 
-  outElastic: function(){
-    return function(k){
-      let s, a = 0.1, p = 0.4;
-      if ( k === 0 ) return 0;
-      if ( k === 1 ) return 1;
+  outElastic: function(a = 0.1,p = 0.4){
+    return function(t){
+      let s;
+      if ( t === 0 ) return 0;
+      if ( t === 1 ) return 1;
       if ( !a || a < 1 ) { a = 1; s = p / 4; }
       else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
-      return ( a * Math.pow( 2, - 10 * k) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) + 1 );
+      return ( a * Math.pow( 2, - 10 * t) * Math.sin( ( t - s ) * ( 2 * Math.PI ) / p ) + 1 );
     };
   },
 
-  inOutElastic: function(){
-    return function(k){
-      let s, a = 0.1, p = 0.4;
-      if ( k === 0 ) return 0;
-      if ( k === 1 ) return 1;
+  inOutElastic: function(a = 0.1,p = 0.4){
+    return function(t){
+      let s;
+      if ( t === 0 ) return 0;
+      if ( t === 1 ) return 1;
       if ( !a || a < 1 ) { a = 1; s = p / 4; }
       else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
-      k *= 2;
-      if ( k < 1 ) return - 0.5 * ( a * Math.pow( 2, 10 * ( k - 1 ) ) * Math.sin( ( (k-1) - s ) * ( 2 * Math.PI ) / p ) );
-      return a * Math.pow( 2, -10 * ( k - 1 ) ) * Math.sin( ( (k-1) - s ) * ( 2 * Math.PI ) / p ) * 0.5 + 1;
+      t *= 2;
+      if ( t < 1 ) return - 0.5 * ( a * Math.pow( 2, 10 * ( t - 1 ) ) * Math.sin( ( (t-1) - s ) * ( 2 * Math.PI ) / p ) );
+      return a * Math.pow( 2, -10 * ( t - 1 ) ) * Math.sin( ( (t-1) - s ) * ( 2 * Math.PI ) / p ) * 0.5 + 1;
     };
   },
 
   inBack: function(v){
-    return function(k){
+    return function(t){
       let s = v || 1.70158;
-      return k * k * ( ( s + 1 ) * k - s );
+      return t * t * ( ( s + 1 ) * t - s );
     };
   },
 
   outBack: function(v){
-    return function(k){
+    return function(t){
       let s = v || 1.70158;
-      return --k * k * ( ( s + 1 ) * k + s ) + 1;
+      return --t * t * ( ( s + 1 ) * t + s ) + 1;
     };
   },
 
   inOutBack: function(v){
-    return function(k){
+    return function(t){
       let s =  (v || 1.70158) * 1.525;
-      k *= 2;
-      if ( k < 1 ) return 0.5 * ( k * k * ( ( s + 1 ) * k - s ) );
-      return 0.5 * ( ( k - 2 ) * (k-2) * ( ( s + 1 ) * (k-2) + s ) + 2 );
+      t *= 2;
+      if ( t < 1 ) return 0.5 * ( t * t * ( ( s + 1 ) * t - s ) );
+      return 0.5 * ( ( t - 2 ) * (t-2) * ( ( s + 1 ) * (t-2) + s ) + 2 );
     };
   },
 
   inBounce: function(){
-    return function(k){
-      return 1 - Easing.outBounce()( 1 - k );
+    return function(t){
+      return 1 - Easing.outBounce()( 1 - t );
     };
   },
 
   outBounce: function(){
-    return function(k){
-      if ( k < ( 1 / 2.75 ) ) {
-        return 7.5625 * k * k;
-      } else if ( k < ( 2 / 2.75 ) ) {
-        k = ( k - ( 1.5 / 2.75 ) );
-        return 7.5625 * k * k + 0.75;
-      } else if ( k < ( 2.5 / 2.75 ) ) {
-        k = (k - ( 2.25 / 2.75 ));
-        return 7.5625 * k * k + 0.9375;
+    return function(t){
+      if ( t < ( 1 / 2.75 ) ) {
+        return 7.5625 * t * t;
+      } else if ( t < ( 2 / 2.75 ) ) {
+        t = ( t - ( 1.5 / 2.75 ) );
+        return 7.5625 * t * t + 0.75;
+      } else if ( t < ( 2.5 / 2.75 ) ) {
+        t = (t - ( 2.25 / 2.75 ));
+        return 7.5625 * t * t + 0.9375;
       } else {
-        k -= ( 2.625 / 2.75 );
-        return 7.5625 * k * k + 0.984375;
+        t -= ( 2.625 / 2.75 );
+        return 7.5625 * t * t + 0.984375;
       }
     };
   },
 
   inOutBounce: function(){
-    return function(k){
-      if ( k < 0.5 ) return Easing.inBounce()( k * 2 ) * 0.5;
-      return Easing.outBounce()( k * 2 - 1 ) * 0.5 + 0.5;
+    return function(t){
+      if ( t < 0.5 ) return Easing.inBounce()( t * 2 ) * 0.5;
+      return Easing.outBounce()( t * 2 - 1 ) * 0.5 + 0.5;
     };
+  },
+
+  customArray: function(arr){
+    if(!arr)return Easing.linear();
+    return function(t){
+      //todo: convert array => ease
+      return t;
+    }
   }
 };
 
